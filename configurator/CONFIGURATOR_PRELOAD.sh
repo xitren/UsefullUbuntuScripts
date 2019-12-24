06c232ee3f0b2df99b4e285c3ab13446b89a4834
xitren <xitren@ya.ru>
2019-12-24 05:42:04 +0300

    -

---
+++ configurator/CONFIGURATOR_PRELOAD.sh
@@ -0,0 +1,4 @@
+sudo ip route del default via 192.168.55.1
+sudo apt update -y && sudo apt upgrade -y
+sudo ip route del default via 192.168.55.1
+sudo apt install dos2unix git sudo vim nano perl libwww-mechanize-shell-perl perl-mechanize nmap -y
