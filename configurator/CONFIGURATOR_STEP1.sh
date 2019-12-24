4494ec0e97b5ba83608c38b7e8a71b0189973c9d
xitren <xitren@ya.ru>
2019-12-24 06:04:31 +0300

    -

--- configurator/CONFIGURATOR_STEP1.sh
+++ configurator/CONFIGURATOR_STEP1.sh
@@ -1,7 +1,8 @@
 ssh-keygen -f ~/.ssh/id_ecdsa -t ecdsa -b 521
 ssh-copy-id -i ~/.ssh/id_ecdsa sdwayne@192.168.55.2
 cd ../server
-scp -vr * 'sdwayne@192.168.55.2:~/'
+ssh 'sdwayne@192.168.55.2' "mkdir server"
+scp -vr * 'sdwayne@192.168.55.2:~/server'
 ssh 'sdwayne@192.168.55.2' "ls -al ~/"
 #sdwayne ALL=(ALL) NOPASSWD:ALL
 ssh -t 'sdwayne@192.168.55.2' "sudo nano /etc/sudoers"
---
+++ server/STEP7_SERVER_SENTRY_CONFIGS.sh
@@ -0,0 +1,2 @@
+sudo cp jail.local /etc/fail2ban/jail.local
+sudo cp portsentry.conf /etc/portsentry/portsentry.conf
